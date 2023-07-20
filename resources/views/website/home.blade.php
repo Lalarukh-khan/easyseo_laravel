a659b1a84edb4977f2e0d92de2dcbeec2fb70129
Lalarukh Khan <lalarukhkhan179@gmail.com>
2023-07-20 11:18:54 +0500

    Same blog height

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -1821,7 +1821,7 @@
                                                             <p class="pad-bot-20 p-l-20 p-r-20 blkchnpara">
                                                                 <span class="blkchn">{{ $blog->category->name }}
                                                                 </span>&nbsp; &nbsp; &nbsp; 5 min read</p>
-                                                                <a class="p-l-20 p-r-20 font-size-20 nwacolc blgtthgt" href="{{ route('web.blog.details',$blog->slug) }}"> {{ $blog->title }} </a>
+                                                                <a class="p-l-20 p-r-20 font-size-20 nwacolc blgtthgt " href="{{ route('web.blog.details',$blog->slug) }}"> {{ $blog->title }} </a>
                                                              <p
                                                                 class="pad-bot-15 font-size-15 font-weight-lighter p-l-20 p-r-20 lrm">
                                                                 {{Str::limit(strip_tags($blog->description,150))}} </p>
