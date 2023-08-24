1da2b73d33339d6087a269a53d6a5017875ced8f
Lalarukh Khan <lalarukhkhan179@gmail.com>
2023-08-24 17:09:06 +0500

    Email reverting

---
+++ resources/views/email/suscription_cancel.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>{{config('app.name')}}</title>
+</head>
+<body>
+    <h1>{{ $details['title'] }}</h1>
+    @if (isset($details['subject']) && !empty($details['subject']))
+    <p>{{$details['subject']}}</p>
+    @endif
+    <p>{{ $details['body'] }}</p>
+    <br> 
+
+    <p>Thank you</p>
+</body>
+</html>
